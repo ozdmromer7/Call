3c8a55c3044c4c2b09553210f0f78849e2ba9a0a
Ömer Bartu Özdemir <97477582+omeer7@users.noreply.github.com>
2022-10-14 10:05:07 +0300

    First

--- app/src/main/java/com/eryaz/androidwidgets/MainActivity.kt
+++ app/src/main/java/com/eryaz/androidwidgets/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.eryaz.androidwidgets
 
 import android.Manifest
+import android.content.Context
 
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -10,9 +11,11 @@ import android.os.Bundle
 import android.provider.CallLog
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.properties.Delegates
 
 
 class MainActivity : AppCompatActivity() {
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,21 +26,33 @@ class MainActivity : AppCompatActivity() {
 
         checkPermission()
 
-        imageView.setOnClickListener {
+        call.setOnClickListener {
+
 
             val number = editTextPhone.text.toString()
 
-            if (number.isNotEmpty()) {
-                val intent = Intent(Intent.ACTION_CALL)
-                intent.data = Uri.parse("tel:$number")
-                startActivity(intent)
+            if (it.id == R.id.call) {
+
+                if (number.isNotEmpty()) {
+                    val intent = Intent(Intent.ACTION_CALL)
+                    intent.data = Uri.parse("tel:$number")
+                    startActivity(intent)
+
+                }
             }
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+    }
+
     override fun onResume() {
         super.onResume()
-        getCallDetails()
+
+            getCallDetails()
+        
     }
     fun checkPermission(){
         if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
@@ -52,32 +67,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun getCallDetails(){
-        val sb =StringBuffer()
+
+        //val sp = this.getSharedPreferences("Last Call", Context.MODE_PRIVATE)
+       // val editor = sp.edit()
+
         val contacts = CallLog.Calls.CONTENT_URI
         val cursor = this.contentResolver.query(contacts,null,null,null,null)
         val duration = cursor?.getColumnIndex(CallLog.Calls.DURATION)
-        val date = cursor?.getColumnIndex(CallLog.Calls.DATE)
         val number = cursor?.getColumnIndex(CallLog.Calls.CACHED_NAME)
-        sb.append("Call Details: ")
 
         if (cursor != null) {
-             var x = 0
+             var lastCall = 0
              var name = ""
             while (cursor.moveToNext()){
 
-
                 val durationtime = duration?.let { cursor.getString(it) }
-                val date = date?.let { cursor.getString(it) }
                 val number = number?.let { cursor.getString(it) }
 
-                sb.append("\nDuration Time"+ durationtime)
-                x = durationtime.toString().toInt()
+                lastCall = durationtime.toString().toInt()
                 name = number.toString()
-
             }
             cursor.close()
-            println(x.toString())
-            textView.setText(x.toString()+"seconds by "+ name)
+
+            //editor.putInt("Duration",lastCall)
+            //editor.apply()
+
+            //val t=sp.getString("Duration","0")
+
+            textView.setText(lastCall.toString() +" seconds \nCalled "+ name)
 
         }
 
